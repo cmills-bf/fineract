a3c531644d5c295920e9fb42d334f2d49a5fed5b
Chris Mills <cmills@breakfreesolutions.com>
2023-07-26 12:58:00 -0500

    hid existing tests, modified build settings, seems to have removed cucumber error

--- fineract-provider/src/test/java/org/apache/fineract/TestSuite.java
+++ fineract-provider/src/test/java/org/apache/fineract/TestSuite.java
@@ -23,6 +23,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+// import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @CucumberContextConfiguration
